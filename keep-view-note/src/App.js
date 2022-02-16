7ba01f83e5503005ba88966b5e1c3aeb37557f77
the-wolvorine <uday.cheekatipalli@slu.edu>
2022-02-16 15:05:35 -0600

    Created a new react app

---
+++ keep-view-note/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Keep View Note
+      </header>
+    </div>
+  );
+}
+
+export default App;
