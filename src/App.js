d876765f5a78a08e990ee1cd2e1b4e610df11ab1
Hruday kumar Bollepally <hruday@Hrudays-MBP.attlocal.net>
2022-02-21 12:56:07 -0600

    Added a new route for about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import ViewCount from "./ViewCount.jsx";
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import About from "./About";
 
 function App() {
   return (
     <div>
-    <center><ViewCount/></center>
-    <div>
+    <Router>
+           <Routes>
+                 <Route exact path='/' element={< ViewCount />}></Route>
+                 <Route exact path='/about' element={<About/>}></Route>
+          </Routes>
+    </Router>
     </div>
-    </div>
+    
+    
+    
   );
 }
   
