547e84867e5a3dd0eacb3a2d754d4e8046fb7ac4
sushma-1997 <sushmachitturi@Sushmas-MBP.attlocal.net>
2022-02-21 13:41:09 -0600

    Added bootstrap and created header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import ViewCount from "./ViewCount.jsx";
+import Header from "./Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <center><ViewCount/></center>
     </div>
   );
