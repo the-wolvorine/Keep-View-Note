a0ba901ce2bd9e99d331d73dae7e85e01a021490
sushma-1997 <sushmachitturi@Sushmas-MBP.attlocal.net>
2022-03-04 19:20:35 -0600

    added ui for sign up and login page and add route to login from sign up

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,6 +1,9 @@
 import db from "./config";
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import "./Register.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import Login from "./Login";
   
 function Register() {
   const [userName, setUserName] = useState("");
@@ -40,52 +43,40 @@ function Register() {
   function alertF(){
       alert("Registered Succesfully")
   }
+
   return (
-    <div className="App">
-    <div className="App__form">
-             <form>
-Name: 
-    <div className="row align-items-center g-3">
-      <input
-        type="text"
-        className="row align-items-center g-3"
-        placeholder="Name"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      /></div>
-Email: 
-      <div className="row align-items-center g-3">
-      <input
-        type="text"
-        className="row align-items-center g-3"
-        placeholder="email"
-        value={userEmail}
-        onChange={(e) => setUserEmail(e.target.value)}
-      /></div>
-Mobile Number: 
-      <div className="row align-items-center g-3">
-      <input
-        type="number"
-        className="row align-items-center g-3"
-        placeholder="Mobile number"
-        value={userMobile}
-        onChange={(e) => setUserMobile(e.target.value)}
-      /></div>
-Create new Password: 
-      <div className="row align-items-center g-3">
-      <input
-        type="password"
-        placeholder="Password"
-        value={userPassword}
-        onChange={(e) => setUserPassword(e.target.value)}
-      /></div>
-<br/>
-      <button onClick={submit} className="btn btn-primary">Register</button>
-      
-      </form>
+    <div class="mask d-flex align-items-center gradient-custom-3">
+      <div class="container">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+            <div class="card" style={{borderRadius: '15px' ,margin: '1rem'}}>
+              <div class="card-body p-5">
+                <h2 class="text-capitalize text-center p-2">Create your account</h2>
+                <form>
+                  <div class="form-outline mb-4">
+                    <input type="text" id="form3Example1cg" class="form-control form-control-lg" placeholder="Full Name" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="email" id="form3Example3cg" class="form-control form-control-lg" placeholder="Email" value={userEmail} onChange={(e) => setUserEmail(e.target.value)}/> 
+                  </div> 
+                  <div class="form-outline mb-4">
+                    <input type="tel" id="form3Example4cdg" class="form-control form-control-lg" placeholder="Mobile" value={userMobile} onChange={(e) => setUserMobile(e.target.value)}/>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="password" id="form3Example4cg" class="form-control form-control-lg" placeholder="Password"  value={userPassword} onChange={(e) => setUserPassword(e.target.value)}/>  
+                  </div>
+                  <div class="d-flex justify-content-center">
+                    <button type="submit" onClick={submit} class="btn btn-block btn-lg btn-dark">Register</button>
+                  </div>
+                  <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="/login" onClick={<Login/>} class="fw-bold text-body"><u>Login here</u></a></p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
 }
   
 export default Register;
