699fa83e61bb96e461a8682719ce7474bd416d4f
Hruday kumar Bollepally <hruday@Hrudays-MBP.attlocal.net>
2022-04-10 20:36:50 -0500

    Add new feature to encrypt notes, which requires a passcode to read

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -12,12 +12,20 @@ function Welcome(){
     const [name, setName] = useState("");
     const [textarea, setTextarea] = useState("");
     const [textarea1, setTextarea1] = useState("");
+    const [textarea2, setTextarea2] = useState("");
     const [clicked,setClicked] = useState(false);
     const [notes, setNotes] = useState({});
+    const [lockednotes, setLockedNotes] = useState({});
     const [id,setId] = useState("")
     const [clickedViewNote,setClickedViewNote] = useState(false);
     const [clickedEditNote,setClickedEditNote] = useState(false);
     const [editnote, setEditNote] = useState("");
+    const [clickedEditNoteLocked,setClickedEditNoteLocked] = useState(false);
+    const [editnotelocked, setEditNoteLocked] = useState("");
+    const [unlocknotes, setUnlockNotes] = useState(false);
+    const [userPassword, setUserPassword] = useState("");
+    const [originalPassword, setOriginalPassword] = useState("");
+    const [unlockSuccess , setUnlockSuccess] = useState(false);
 
     toast.configure()
     const notify = ()=>{
@@ -34,6 +42,28 @@ function Welcome(){
             setClicked(false);
         }
       },[clicked])
+
+      useEffect(()=>{
+        if(unlockSuccess===true)
+        {
+            setUnlockSuccess(true);
+        }
+        if(unlockSuccess===false)
+        {
+            setUnlockSuccess(false);
+        }
+      },[unlockSuccess])
+
+      useEffect(()=>{
+        if(unlocknotes===true)
+        {
+            setUnlockNotes(true);
+        }
+        if(unlocknotes===false)
+        {
+            setUnlockNotes(false);
+        }
+      },[unlocknotes])
 
     useEffect(()=>{
         if(clickedViewNote===true)
@@ -56,6 +86,17 @@ function Welcome(){
             setClickedEditNote(false);
         }
       },[clickedEditNote])
+
+      useEffect(()=>{
+        if(clickedEditNoteLocked===true)
+        {
+            setClickedEditNoteLocked(true);
+        }
+        if(clickedEditNoteLocked===false)
+        {
+            setClickedEditNoteLocked(false);
+        }
+      },[clickedEditNoteLocked])
       
     useEffect(() => {
         db.collection("usersData")
@@ -67,12 +108,23 @@ function Welcome(){
                   setName(element.data().name)
                   setNotes(element.data().notes)
                   setId(element.id)
+                  setLockedNotes(element.data().noteslocked)
+                  setOriginalPassword(element.data().password)
               }    
           });
         })) 
         }, []);
 
+    function update(){
+      db.collection("usersData").doc(id).get().then((function(doc){
+        setLockedNotes(doc.data().noteslocked)
+        setNotes(doc.data().notes)
+        }))
+    }
+
     function handleChange(){
+      if(textarea.length>0)
+      {
         db.collection("usersData").doc(id).set({
           "notes": firebase.firestore.FieldValue.arrayUnion(textarea)
         },
@@ -81,7 +133,11 @@ function Welcome(){
         setClicked(false)
         db.collection("usersData").doc(id).get().then((function(doc){
         setNotes(doc.data().notes)
-        })) 
+        })) }
+        else{
+          toast.error('Please write something to save', { position: toast.POSITION.BOTTOM_CENTER, autoClose:2000})
+        }
+        setTextarea("")
       }
 
       function handleChangeEdit(){
@@ -100,6 +156,23 @@ function Welcome(){
         toast.success('Note Updated Succesfully', { position: toast.POSITION.BOTTOM_CENTER, autoClose:2000})
         setTextarea1("")
       }
+
+      function handleChangeEditLocked(){
+        db.collection("usersData").doc(id).set({
+            "noteslocked": firebase.firestore.FieldValue.arrayRemove(editnotelocked)
+          },
+          {merge:true})
+        db.collection("usersData").doc(id).set({
+          "noteslocked": firebase.firestore.FieldValue.arrayUnion(textarea2)
+        },
+        {merge:true})
+        setClickedEditNoteLocked(false)
+        db.collection("usersData").doc(id).get().then((function(doc){
+        setLockedNotes(doc.data().noteslocked)
+        })) 
+        toast.success('Note Updated Succesfully', { position: toast.POSITION.BOTTOM_CENTER, autoClose:2000})
+        setTextarea2("")
+      }
         
     function addNotes(){
         setClicked(true);
@@ -111,6 +184,8 @@ function Welcome(){
 
     function cancelviewNote(){
         setClickedViewNote(false)
+        setUnlockNotes(false)
+        setUnlockSuccess(false)
      }
 
     function cancelChange(){
@@ -121,10 +196,19 @@ function Welcome(){
         setClickedEditNote(false)
     }
 
+    function cancelChangeEditLocked(){
+      setClickedEditNoteLocked(false)
+  }
+
     function editNote(note){
         setEditNote(note)
         setClickedEditNote(true)
     }
+
+    function editNoteLocked(note){
+      setEditNoteLocked(note)
+      setClickedEditNoteLocked(true)
+  }
 
     function delNote(note){
         db.collection("usersData").doc(id).set({
@@ -134,7 +218,61 @@ function Welcome(){
           toast.warning('Note deleted Succesfully', { position: toast.POSITION.BOTTOM_CENTER, autoClose:2000})
           db.collection("usersData").doc(id).get().then((function(doc){
             setNotes(doc.data().notes)
+            setLockedNotes(doc.data().noteslocked)
             })) 
+    }
+
+    function delNoteLocked(note){
+      db.collection("usersData").doc(id).set({
+        "noteslocked": firebase.firestore.FieldValue.arrayRemove(note)
+      },
+      {merge:true})
+      toast.warning('Note deleted Succesfully', { position: toast.POSITION.BOTTOM_CENTER, autoClose:2000})
+      db.collection("usersData").doc(id).get().then((function(doc){
+        setLockedNotes(doc.data().noteslocked)
+        setNotes(doc.data().notes)
+        })) 
+    }
+
+    function lockNotes(note){
+      db.collection("usersData").doc(id).set({
+        "notes": firebase.firestore.FieldValue.arrayRemove(note)
+      },
+      {merge:true})
+      db.collection("usersData").doc(id).set({
+        "noteslocked": firebase.firestore.FieldValue.arrayUnion(note)
+      },
+      {merge:true})
+      update();
+    }
+
+    function unlockNotes(){
+      setUnlockNotes(true);
+    }
+
+    function lockAgain(){
+      setUnlockNotes(false)
+      setUnlockSuccess(false)
+    }
+
+    function unlockClicked(){
+      if(userPassword===originalPassword)
+      setUnlockSuccess(true);
+      else
+      toast.error('Sorry, Invalid Passsword!!', { position: toast.POSITION.BOTTOM_CENTER, autoClose:2000})
+      setUserPassword("")
+    }
+
+    function unlockNotesRemove(note){
+      db.collection("usersData").doc(id).set({
+        "noteslocked": firebase.firestore.FieldValue.arrayRemove(note)
+      },
+      {merge:true})
+      db.collection("usersData").doc(id).set({
+        "notes": firebase.firestore.FieldValue.arrayUnion(note)
+      },
+      {merge:true})
+       update();
     }
     
     return(
@@ -176,12 +314,42 @@ function Welcome(){
                 <table class="table table-bordered table-hover">
                 <thead><b><i>NOTES</i></b></thead>
                 <tbody>{notes.map((notes)=>
-                <tr><li>{notes}<td><button class="btn btn-outline-dark" onClick={() => {editNote(notes);setTextarea1(notes)}}>Update</button>&nbsp;<button class="btn btn-dark" onClick={() => delNote(notes)}>Delete</button></td>
+                <tr><li>{notes}<td>
+                  <button class="btn btn-outline-dark" onClick={() => {editNote(notes);setTextarea1(notes)}}>Update</button>&nbsp;
+                  <button class="btn btn-outline-dark" onClick={() => lockNotes(notes)}>Lock</button>&nbsp;
+                  <button class="btn btn-dark" onClick={() => delNote(notes)}>Delete</button></td>
                 </li></tr>)
                 }</tbody></table>
             </div>}
+  {/* Encrypted Notes start*/}
+            {clickedEditNoteLocked &&<div><b>Update Your Note Here</b><div className="input-group">
+            <textarea value={textarea2} onChange={(e) => setTextarea2(e.target.value)} className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+            </div> 
+                <br/><button class="btn btn-outline-dark" onClick={handleChangeEditLocked}>Submit</button>&nbsp;
+                <button class="btn btn-dark" onClick={cancelChangeEditLocked}>Cancel</button></div>} 
+                {clickedViewNote && !unlockSuccess && <b><button class="btn btn-outline-dark" onClick={unlockNotes}>ENCRYPTED NOTES <img src="https://media.istockphoto.com/vectors/lock-icon-vector-id936681148?k=20&m=936681148&s=612x612&w=0&h=j6fxNWrJ09iE7khUsDWetKn_PwWydgIS0yFJBEonGow=" height="30"/></button></b>}
+                {clickedViewNote && unlocknotes && <div>
+                  <div class="form-outline mb-4">
+                  {!unlockSuccess && <div><br/><input type="password" id="form3Example4cg" class="form-control form-control-lg" placeholder="Password"  value={userPassword} onChange={(e) => setUserPassword(e.target.value)} onKeyPress={(e) => { if (e.key === "Enter") { unlockClicked();}}}/></div>}
+                  </div>
+                  {!unlockSuccess && <div class="d-flex justify-content-center"><button onClick={unlockClicked} className="btn btn-block btn-lg btn-dark">unlock</button> </div>}
+                      {unlockSuccess && <div>
+                    <table class="table table-bordered table-hover">
+                    <thead><b><i>ENCRYPTED NOTES &nbsp;<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCswczXCVvOOzNq90KITbZeWGTuN1LukqAeA&usqp=CAU" height="20"/></i></b></thead>
+                    <tbody>{lockednotes.map((notes)=>
+                    <tr><li>{notes}<td>
+                      <button class="btn btn-outline-dark" onClick={() => {editNoteLocked(notes);setTextarea2(notes)}}>Update</button>&nbsp;
+                      <button class="btn btn-outline-dark" onClick={() => unlockNotesRemove(notes)}>Unlock</button>&nbsp;
+                      <button class="btn btn-dark" onClick={() => delNoteLocked(notes)}>Delete</button></td>
+                    </li></tr>)
+                    }</tbody></table>
+                        </div>}
+                </div>}
+                {clickedViewNote && unlocknotes &&  unlockSuccess && <div><button class="btn btn-dark" onClick={lockAgain}>Hide</button></div>}
+            
             {clickedViewNote && <div><button class="btn btn-dark" onClick={cancelviewNote}>Close</button></div>}
             </div>
+            {/* Encrypted Notes end*/}
         </div>
     );
 }
